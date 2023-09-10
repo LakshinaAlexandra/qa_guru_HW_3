0c8ea1d3990e67384df365b709d09cd2f5868092
Aleksandra Lakshina <aleksandra.lakshina@gmail.com>
2023-09-10 20:26:19 +0300

    Fixed file selection

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -17,7 +17,10 @@ public class TextBoxTests {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
-
+// Remove banner
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+//
         $("#firstName").setValue("Aleksandra");
         $("#lastName").setValue("Lak");
         $("#userEmail").setValue("AleksandraLak@example.com");
@@ -27,10 +30,8 @@ public class TextBoxTests {
         $("#userNumber").setValue("9997776655");
 // Дата
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").click();
-        $(byText("May")).click();
-        $(".react-datepicker__year-select").click();
-        $(byText("1970")).click();
+        $(".react-datepicker__month-select").$(byText("May")).click();
+        $(".react-datepicker__year-select").$(byText("1970")).click();
         $(".react-datepicker__day.react-datepicker__day--020").click();
 // Multiple select
         $("#subjectsInput").setValue("n");
@@ -40,29 +41,29 @@ public class TextBoxTests {
 // CheckBox
         $(byText("Sports")).click();
 // Файл
-        $("#uploadPicture").sendKeys("/Users/malekov/Desktop/Тестовые файлы/олень.jpeg");
+        //$("#uploadPicture").sendKeys("/Users/malekov/Desktop/Тестовые файлы/олень.jpeg");
+        $("#uploadPicture").uploadFromClasspath("олень.jpeg");
 // Адрес
         $("#currentAddress").setValue("My current address");
 // Drop down list - 1
-        $x("(//div[contains(@class, 'css-tlfecz-indicatorContainer')])[2]").click();
+        $("#state").click();
         $(byText("Haryana")).click();
-// Клик в пустое место
-        $("#stateCity-label").click();
 // Drop down list - 2
-        $x("(//div[contains(@class, 'css-tlfecz-indicatorContainer')])[3]").click();
+        $("#city").click();
         $(byText("Panipat")).click();
 // Сохранение
         $("#submit").click();
 // Проверки
-        $x("//tbody//tr[1]").shouldHave(text("Aleksandra Lak"));
-        $x("//tbody//tr[2]").shouldHave(text("AleksandraLak@example.com"));
-        $x("//tbody//tr[3]").shouldHave(text("Female"));
-        $x("//tbody//tr[4]").shouldHave(text("9997776655"));
-        $x("//tbody//tr[5]").shouldHave(text("20 May,1970"));
-        $x("//tbody//tr[6]").shouldHave(text("Hindi, Biology"));
-        $x("//tbody//tr[7]").shouldHave(text("Sports"));
-        $x("//tbody//tr[8]").shouldHave(text("олень.jpeg"));
-        $x("//tbody//tr[9]").shouldHave(text("My current address"));
-        $x("//tbody//tr[10]").shouldHave(text("Haryana Panipat"));
+        $(".modal-content").shouldHave(
+                text("Aleksandra Lak"),
+                text("AleksandraLak@example.com"),
+                text("Female"),
+                text("9997776655"),
+                text("20 May,1970"),
+                text("Hindi, Biology"),
+                text("Sports"),
+                text("олень.jpeg"),
+                text("My current address"),
+                text("Haryana Panipat"));
     }
 }
